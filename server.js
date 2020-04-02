d27858e88815957942139adbdffb0698e0b8ae77
T1p0un3t <44785302+T1p0un3t@users.noreply.github.com>
2020-04-02 22:00:52 +0200

    Warning & error correction
    Update dependencie for security
    Clean code
    Add tsconfig.json

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const DBSOURCE = "db.users.sqlite";
 let db = new sqlite3.Database(DBSOURCE, (err) => {
   if (err) {
     // Cannot open database
-    console.error(err.message)
-    throw err
+    console.error(err.message);
+    throw err;
   } else {
     console.log('Connected to the SQLite database.');
 
